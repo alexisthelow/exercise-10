64c8be36d67ca75652521c3d95ce32f385392d3a
Alexis Low <alexisthelow@gmail.com>
2017-08-04 14:36:27 -0600

    fixed it

--- Sequencing.java
+++ Sequencing.java
@@ -4,13 +4,13 @@ public class Sequencing {
 	public static void main(String[] args) {
 		
 		Scanner keyboard = new Scanner(System.in);
-		double price = 0, salesTax, total;
-		
-		salesTax = price * 0.0825;
-		total = price + salesTax;
+		double price, salesTax, total;
 		
 		System.out.print("How much is the purchase price? ");
 		price = keyboard.nextDouble();
+		
+		salesTax = price * 0.0825;
+		total = price + salesTax;
 		
 		System.out.println("Item price:\t" + price);
 		System.out.println("Salex tax:\t" + salesTax);
